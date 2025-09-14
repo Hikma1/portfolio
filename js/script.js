e6b8eca77ad9d521142cd55afded8e69bab7d8a8
Hikma1 <hikma0227@gmail.com>
2025-09-14 18:44:16 +0300

    adding scroll js

--- js/script.js
+++ js/script.js
@@ -28,3 +28,23 @@ let letter = "";
   }
 }());
 
+// Scroll Reveal
+function revealOnScroll() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 80; // trigger distance
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active"); // optional (re-animate on scroll back)
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll(); // trigger on load
+
