1d7768a5acf67c71533942fbf071837f1b933e91
Hikma1 <hikma0227@gmail.com>
2025-11-20 21:43:50 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const words = [
+  "السلام عليكم 👋",        // Arabic
+  "Hello there 👋",         // English
+  "Bonjour 👋",             // French
+  "ሰላም እንዴት ነሽ 👋",      // Amharic
+  "你好 👋",                 // Chinese
+];
